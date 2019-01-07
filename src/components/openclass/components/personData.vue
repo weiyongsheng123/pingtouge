0e8c6d5d22d93fbeb8e9ca2d08122e0ab94f4be6
weiyongsheng123 <846752569@qq.com>
2019-01-07 11:51:14 +0800

    a openclass persondata

--- src/components/openclass/components/personData.vue
+++ src/components/openclass/components/personData.vue
@@ -8,8 +8,29 @@
     </div>
     <div class="data">
        <div class="input-container">
+         <input type="text" name="name" required/>
          <label for="name">姓名</label>
-         <input type="text" id="name"/>
+         <div class="bottom-line"></div>
+       </div>
+       <div class="input-select">
+         <input type="radio" name="sex" value="男"/>男
+         <input type="radio" name="sex"  value="女"/>女
+         <label for="sex">性别</label>
+         <div class="bottom-line"></div>
+       </div>
+       <div class="input-container">
+         <input type="text" name="school" required/>
+         <label for="school">就读学校</label>
+         <div class="bottom-line"></div>
+       </div>
+       <div class="input-container">
+         <input type="text" name="phone" required/>
+         <label for="phone">联系方式</label>
+         <div class="bottom-line"></div>
+       </div>
+        <div class="input-container">
+         <input type="text" name="date" required/>
+         <label for="date">时间</label>
          <div class="bottom-line"></div>
        </div>
     </div>
@@ -46,4 +67,72 @@
           position: absolute
           left: 20px
           top: -55px
+    .data
+      .input-container
+        width: 20%
+        height: 50px
+        margin-top: 30px
+        margin-bottom: 30px
+        margin-left: 10%
+        font-size: 23px
+        line-height: 1.0
+        display: inline-block
+        position: relative
+        .bottom-line
+          width: 100%
+          height: 2px
+          background: #000
+        label
+          font-size: 20px
+          line-height: 1.0
+          font-weight: 400
+          position: absolute
+          left: 0px
+          top: 15px
+        input
+          outline: none
+          width: 100%
+          height: 100%
+          font-size: 23px
+          line-height: 1.0
+          &:focus
+            + label
+              transition: all 0.75s
+              transform: translate(-45px,-30px)
+          &:valid
+            + label
+              transition: all 0.75s
+              transform: translate(-45px,-30px)
+      .input-select
+        width: 20%
+        height: 50px
+        margin-top: 30px
+        margin-bottom: 30px
+        margin-left: 10%
+        font-size: 20px
+        line-height: 1.0
+        display: inline-block
+        position: relative
+        padding: 10px 0 0 0
+        input
+          width: 20px
+          height: 20px
+          margin-left: 10px
+          margin-right: 5px
+          &:checked
+            ~ label
+              transition: all 0.75s
+              transform: translateY(-30px)
+        label
+          font-size: 20px
+          line-height: 1.0
+          font-weight: 400
+          position: absolute
+          left: -45px
+          top: 15px
+        .bottom-line
+          width: 100%
+          height: 2px
+          background: #000
+          margin-top: 10px
 </style>
